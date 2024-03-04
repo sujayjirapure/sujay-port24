01d243db32c6aa2194678df104eef2ab6ecd7c4c
sujay <jirapuresujay@gmail.com>
2024-03-04 19:00:27 +0530

    5th

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -8,6 +8,7 @@ import editor from "../../Assets/Projects/codeEditor.png";
 import chatify from "../../Assets/Projects/chatify.png";
 import suicide from "../../Assets/Projects/suicide.png";
 import bitsOfCode from "../../Assets/Projects/blog.png";
+import bitsOfCodenew from "../../Assets/Projects/sujay-port.png";
 
 function Projects() {
   return (
@@ -23,12 +24,12 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={bitsOfCodenew}
               isBlog={false}
-              title="Chatify"
+              title="Portfolio wedsite"
               description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              ghLink="https://github.com/sujayjirapure/sujay-port24"
+              demoLink="https://sujay-port24.vercel.app/"
             />
           </Col>
 
